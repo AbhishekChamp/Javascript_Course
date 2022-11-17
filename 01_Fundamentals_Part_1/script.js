71b9ff994b6d3eb3aafa023a6580707853d54125
AbhishekChamp <abhishek96.r@gmail.com>
2022-11-17 23:43:54 +0530

    Day-01

---
+++ 01_Fundamentals_Part_1/script.js
@@ -0,0 +1,26 @@
+let welcome = "Hello World";
+console.log((340 * 23) / 43 + 4 - 12);
+
+console.log("Champ");
+console.log(8);
+
+let firstName = "Champ";
+
+console.log(firstName);
+
+// Find type
+console.log(typeof true);
+
+// Coding Challenge #1
+console.log("Coding Challenge #1");
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
