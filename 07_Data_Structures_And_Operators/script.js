4c52feaf163808c25a142ab9e04d29030d74a266
AbhishekChamp <abhishek96.r@gmail.com>
2022-12-18 21:19:22 +0530

    Day-14

--- 07_Data_Structures_And_Operators/script.js
+++ 07_Data_Structures_And_Operators/script.js
@@ -108,3 +108,39 @@ for (const player of game.scored) {
     scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 console.log(scorers);
+
+const gameEvents = new Map([
+    [17, "⚽ GOAL"],
+    [36, "🔁 Substitution"],
+    [47, "⚽ GOAL"],
+    [61, "🔁 Substitution"],
+    [64, "🔶 Yellow card"],
+    [69, "🔴 Red card"],
+    [70, "🔁 Substitution"],
+    [72, "🔁 Substitution"],
+    [76, "⚽ GOAL"],
+    [80, "⚽ GOAL"],
+    [92, "🔶 Yellow card"],
+]);
+
+// Solution - 03
+console.log("--------------SOLUTION-03------------");
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`,
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+    `An event happened, on average, every ${time / gameEvents.size} minutes`,
+);
+
+for (const [min, event] of gameEvents) {
+    const half = min <= 45 ? "FIRST" : "SECOND";
+    console.log(`[${half} HALF] ${min}: ${event}`);
+}
