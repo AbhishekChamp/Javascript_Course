2c0972378ea25c55b142688ef4b53152ab495abf
AbhishekChamp <abhishek96.r@gmail.com>
2023-01-20 23:23:48 +0530

    Day-17

--- 08_Functions/script.js
+++ 08_Functions/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+console.log("For default parameters refer 01_default_parameters.js");
+console.log("For value vs reference refer 02_value_vs_reference.js");
+console.log("For callbacks refer 03_callback.js");
+console.log(
+    "For function returning function refer 04_function_returning_function.js",
+);
+console.log("For call and apply methods refer 05_call_apply.js");
+console.log("For bind method refer 06_bind.js");
+console.log("For challenge 1 refer 07_challenge1.js");
+console.log("For immediately invoked function expression refer 08_iife.js");
+console.log("For closures refer 09_closures.js");
+console.log("For challenge 2 refer 10_challenge2.js");
