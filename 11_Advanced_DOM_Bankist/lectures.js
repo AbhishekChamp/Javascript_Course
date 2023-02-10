aef0e01e25755e5637c02c8401a18810057b475e
AbhishekChamp <abhishek96.r@gmail.com>
2023-02-10 23:52:57 +0530

    Day - 30

--- 11_Advanced_DOM_Bankist/lectures.js
+++ 11_Advanced_DOM_Bankist/lectures.js
@@ -110,3 +110,49 @@ document.querySelector(".nav").addEventListener(
 
 // Stop Propagation
 e.stopPropagation();
+
+// DOM Traversing
+const h1 = document.querySelector("h1");
+
+// Going downwards child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// Going upwards parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// Going sideways: Siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+    if (el !== h1) {
+        el.style.transform = "scale(0.5)";
+    }
+});
+
+// IntersectionObserver API
+const obsCallback = function (entries, observer) {
+    entries.forEach((entry) => {
+        console.log(entry);
+    });
+};
+
+const obsOption = {
+    root: null,
+    threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOption);
+observer.observe(section1);
--- 11_Advanced_DOM_Bankist/script.js
+++ 11_Advanced_DOM_Bankist/script.js
@@ -9,6 +9,11 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
 
 const openModal = function () {
     modal.classList.remove("hidden");
@@ -85,3 +90,62 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
 });
+
+// Tabbed Component
+tabsContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".operations__tab");
+    console.log(clicked);
+    // Guard clause
+    if (!clicked) return;
+
+    // Remove active classes
+    tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+    tabsContent.forEach((c) =>
+        c.classList.remove("operations__content--active"),
+    );
+
+    // Active tab
+    clicked.classList.add("operations__tab--active");
+
+    // Active content area
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add("operations__content--active");
+});
+
+// Menu fade animation
+const handleHover = function (e) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target;
+        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+        const logo = link.closest(".nav").querySelector("img");
+
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+// Sticky Navigation
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    console.log(entry);
+
+    if (!entry.isIntersecting) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
