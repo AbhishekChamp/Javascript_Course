34e1ea0c9922fd273b9dad26c6b1264550f048b6
AbhishekChamp <abhishek96.r@gmail.com>
2023-02-15 23:34:02 +0530

    Day - 35

--- 12_OOP/script.js
+++ 12_OOP/script.js
@@ -36,3 +36,23 @@ console.log(userOne.species, userTwo.species);
 
 console.log(userOne.hasOwnProperty("firstName"));
 console.log(userOne.hasOwnProperty("species"));
+
+console.log(userOne.__proto__.__proto__);
+console.log(userOne.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [1, 2, 3, 4, 5, 4, 5, 2, 5, 7];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((x) => x + 1);
