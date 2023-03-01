281162e3e755a02939c8e654de8083e6f2ffcd7a
AbhishekChamp <abhishek96.r@gmail.com>
2023-03-01 08:38:38 +0530

    Day - 45

--- 15_Modern_JS/script.js
+++ 15_Modern_JS/script.js
@@ -29,3 +29,24 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+    cart: [
+        { product: "bread", quantity: 5 },
+        { product: "pizza", quantity: 5 },
+    ],
+    user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+console.log(stateClone);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
+
+if (module.hot) {
+    module.hot.accept();
+}
